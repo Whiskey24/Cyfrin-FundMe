6ac2ef0ca43750ac34d086a3d7c681f7bd4f0f4f
Whiskey24 <bert@santema.eu>
2025-08-18 22:59:32 +0200

    modifier, receive and fallback added

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,25 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {PriceConverter} from "../libraries/PriceConverter.sol";
+//import {PriceConverter} from "../libraries/PriceConverter.sol";
+import {PriceConverter} from "./PriceConverter.sol";
+
+// 444023 gas
+// 730859 gas
+// 635529 gas with constant
+// 655895 gas without constant  
+
+error NotOwner();
 
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public minimumUsd = 5e18;
+    uint256 public constant MINIMUM_USD = 5e18;
     address[] public funders;
     mapping(address => uint256) public addressToAmountFunded;
 
+    address public immutable i_owner;
+
+    constructor() {
+        i_owner = msg.sender;
+    }
+
     function fund() public payable {
         require(
-            msg.value.getConversionRate() >= minimumUsd,
+            msg.value.getConversionRate() >= MINIMUM_USD,
             "Didn't send enough ETH"
         );
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function withdraw() public {
+    function withdraw() public onlyOwner{
         for (
             uint256 funderIndex = 0;
             funderIndex < funders.length;
@@ -44,5 +58,19 @@ contract FundMe {
             value: address(this).balance
         }("");
         require(callSuccess, "Call failed");
+    }
+
+    modifier onlyOwner() {
+        //require(msg.sender == i_owner, "FundMe__NotOwner");
+        if (msg.sender != i_owner) {revert NotOwner();}
+        _;
+    }
+
+    receive() external payable { 
+        fund();
+    }
+
+    fallback() external payable {
+        fund();
     }
 }
